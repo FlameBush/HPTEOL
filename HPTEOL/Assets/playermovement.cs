19a52da4ef17741c3d603cc5fabbba00b6819876
Meloshke <meloshke.meloshke@gmail.com>
2022-02-03 17:44:35 +0100

    EscapeMenu
    To be finished

--- HPTEOL/Assets/playermovement.cs
+++ HPTEOL/Assets/playermovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class playermovement : MonoBehaviour
@@ -10,6 +11,10 @@ public class playermovement : MonoBehaviour
     public float runSpeed = 40f;
     float horizontal = 0f;
     bool jump = false;
+
+    private bool escapeScreenIsActive = false;
+    public GameObject escapeScreen;
+    public GameObject settingsScreen;
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +31,49 @@ public class playermovement : MonoBehaviour
         {
             jump = true;
         }
-        
+
+        if (escapeScreen != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && !escapeScreenIsActive)
+            {
+                Time.timeScale = 0;
+                escapeScreen.SetActive(true);
+                escapeScreenIsActive = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape) && escapeScreenIsActive)
+            {
+                Time.timeScale = 1;
+                escapeScreen.SetActive(false);
+                escapeScreenIsActive = false;
+            }
+        }
     }
 
     void FixedUpdate()
     {
         controller.Move(horizontal * Time.fixedDeltaTime, false, jump);
         jump = false;
+    }
+
+    public void SettingsScreen()
+    {
+        if (escapeScreenIsActive)
+        {
+            settingsScreen.setActive(true);
+            escapeScreen.setActive(false);
+            escapeScreenIsActive = false;
+            Time.timeScale = 0;
+        }
+    }
+
+    public void BackEscapeScreen()
+    {
+        if (!escapeScreenIsActive)
+        {
+            escapeScreen.setActive(true);
+            escapeScreenIsActive = true;
+            settingsScreen.setActive(false);
+            Time.timeScale = 0;
+        }
     }
 }
