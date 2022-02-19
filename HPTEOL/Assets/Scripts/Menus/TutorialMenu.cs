09a09729bb1fcd709ca38c8ba88130b3af8701e9
SplatTab <skidm44@gmail.com>
2022-02-19 11:37:28 -0600

    Tutorial Scene Setup

--- HPTEOL/Assets/Scripts/Menus/TutorialMenu.cs
+++ HPTEOL/Assets/Scripts/Menus/TutorialMenu.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 public class TutorialMenu : MonoBehaviour
 {
     public int Currentinfo;
+    private Animator playerAnimator;
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+        playerAnimator = GameObject.Find("Player").GetComponent<Animator>();
+        playerAnimator.SetBool("GameIsPaused", true);
+    }
 
     private void Update()
     {
         if (this.gameObject.transform.GetChild(Currentinfo).gameObject.name == "end")
         {
+            Time.timeScale = 1;
+            playerAnimator.SetBool("GameIsPaused", false);
             this.gameObject.SetActive(false);
         }
 
-        if (Input.anyKey)
+        if (Input.anyKeyDown)
         {
             this.gameObject.transform.GetChild(Currentinfo).gameObject.SetActive(false);
             Currentinfo++;
