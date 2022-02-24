fa8d4e2227c5384e11a77f0b71015521c4d8c41f
SplatTab <skidm44@gmail.com>
2022-02-24 16:09:59 -0600

    Level work

--- HPTEOL/Assets/Scripts/Others/Win.cs
+++ HPTEOL/Assets/Scripts/Others/Win.cs
@@ -5,6 +5,8 @@ using System.Collections;
 
 public class Win : MonoBehaviour
 {
+    [SerializeField] GameObject Cutscene;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Player")
@@ -15,17 +17,12 @@ public class Win : MonoBehaviour
 
     public IEnumerator PassLevel(GameObject player)
     {
+        player.SetActive(false);
         int currentLevel = SceneManager.GetActiveScene().buildIndex;
-        if (currentLevel >= PlayerPrefs.GetInt("levelsUnlocked"))
-        {
-            PlayerPrefs.SetInt("levelsUnlocked", currentLevel + 1);
-            player.SetActive(false);
-            GameObject.Find("Healthbar").SetActive(false);
-            GameObject.Find("Spellbar").SetActive(false);
-            GameObject.Find("DiedMenu").SetActive(false);
-            GameObject.Find("Cutscene").SetActive(true);
-            yield return new WaitForSeconds(2);
-            SceneManager.LoadSceneAsync(currentLevel + 1);
-        }
+        PlayerPrefs.SetInt("levelsUnlocked", currentLevel + 1);
+        GameObject.Find("GameCanvas").SetActive(false);
+        Cutscene.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadSceneAsync(currentLevel + 1);
     }
 }
