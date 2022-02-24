c96ddd0bc8b7a3421e5d5c5768a9a0e47450f7f1
SplatTab <skidm44@gmail.com>
2022-02-23 19:17:55 -0600

    Skeleton implemeneted almost

--- HPTEOL/Assets/Scripts/Enemy/BaseEnemy.cs
+++ HPTEOL/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -7,9 +7,10 @@ public class BaseEnemy : MonoBehaviour
     [Range(0f, 100f)]
     [SerializeField] float RandomTurnChance = 2;
     [SerializeField] Transform StartBounds, EndBounds;
+    public int State;
     [Header("Base Stats")]
     [Range(1f, 20f)]
-    [SerializeField] float moveSpeed = 4;
+    public float moveSpeed = 4;
     [Range(1f, 155f)]
     [SerializeField] int Health = 100;
     [Range(4f, 25f)]
@@ -17,20 +18,39 @@ public class BaseEnemy : MonoBehaviour
     [Range(0f, 100f)]
     [SerializeField] int attackDamage = 20;
 
-    private bool MoveRight;
     [HideInInspector] public Transform Player;
-    private bool DamageTimeout;
+    [HideInInspector] public bool DamageTimeout;
+    [HideInInspector] public float firstSpeed;
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public Animator animator;
+    [HideInInspector] public bool moving;
+    [HideInInspector] public string Animation = "Moving";
     private RaycastHit2D Seeing;
-    private int State;
     private SpriteRenderer sprite;
 
-    public virtual void Start()
+    private void Start()
     {
         Player = GameObject.FindWithTag("Player").transform;
         sprite = GetComponent<SpriteRenderer>();
+        firstSpeed = moveSpeed;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.Play("Moving");
     }
 
     public virtual void FixedUpdate()
+    {
+        if (State == 1)
+        {
+            Target();
+        }
+        else if (State == 0)
+        {
+            Roam();
+        }
+    }
+
+    public virtual void Update()
     {
         Seeing = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.left), viewDistance, 8);
         if (Seeing.transform != null && Seeing.transform.CompareTag("Player") && State != -1)
@@ -38,18 +58,11 @@ public class BaseEnemy : MonoBehaviour
             State = 1;
         }
 
-        if (State == 1 && groundCheck())
+        if (moveSpeed == 0)
         {
-            Target();
+            moving = false;
         }
-        else if (State == 0 && groundCheck())
-        {
-            Roam();
-        }
-    }
 
-    private void Update()
-    {
         if (Health <= 0)
         {
             Destroy(gameObject);
@@ -61,22 +74,22 @@ public class BaseEnemy : MonoBehaviour
     /// </summary>
     public virtual void Roam()
     {
-        if (MoveRight)
+        if (sprite.flipX)
         {
-            transform.position = Vector2.MoveTowards(transform.position, EndBounds.position, moveSpeed * Time.deltaTime);
+            rb.MovePosition(Vector2.MoveTowards(transform.position, EndBounds.position, moveSpeed * Time.fixedDeltaTime));
+            moving = true;
             if (Random.Range(0, 100) < RandomTurnChance)
             {
                 sprite.flipX = false;
-                MoveRight = false;
             }
         }
         else
         {
-            transform.position = Vector2.MoveTowards(transform.position, StartBounds.position, moveSpeed * Time.deltaTime);
+            rb.MovePosition(Vector2.MoveTowards(transform.position, StartBounds.position, moveSpeed * Time.fixedDeltaTime));
+            moving = true;
             if (Random.Range(0, 100) < RandomTurnChance)
             {
                 sprite.flipX = true;
-                MoveRight = true;
             }
         }
     }
@@ -86,45 +99,29 @@ public class BaseEnemy : MonoBehaviour
     /// </summary>
     public virtual void Target()
     {
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(Player.position.x, transform.position.y), moveSpeed * Time.deltaTime);
+        rb.MovePosition(Vector2.MoveTowards(transform.position, new Vector2(Player.position.x, transform.position.y), moveSpeed * Time.fixedDeltaTime));
+        moving = true;
         if (Player.position.x > transform.position.x)
         {
             sprite.flipX = true;
-        } else
+        }
+        else
         {
             sprite.flipX = false;
         }
-    }
-
-    [SerializeField] private LayerMask m_WhatIsGround;
-
-    /// <summary>
-    /// Whether or not the enemy is touching the ground.
-    /// </summary>
-    /// <returns>True or False depending on if enemy is touching ground</returns>
-    private bool groundCheck()
-    {
-        Collider2D collider = Physics2D.OverlapBox(transform.position, new Vector2(0.1f, 0.1f), m_WhatIsGround);
-        if (collider != null)
-        {
-            return true;
-        }
-        return false;
     }
 
     public virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("EnemyEndOfWorld"))
         {
-            if (MoveRight)
+            if (sprite.flipX && State == 0)
             {
                 sprite.flipX = false;
-                MoveRight = false;
             }
-            else
+            else if (State == 0)
             {
                 sprite.flipX = true;
-                MoveRight = true;
             }
         }
 
@@ -135,34 +132,34 @@ public class BaseEnemy : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    public virtual void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !DamageTimeout)
         {
-            collision.GetComponent<PlayerStats>().PlayerTakesDamage(attackDamage);
-            StartCoroutine(DamageCooldown());
+            StartCoroutine(DamagePlayer());
         }
     }
 
     /// <summary>
-    /// Puts enemy on a damage cooldown.
+    /// Damages the player and has a damage cooldown starts melee animation and other stuff.
     /// </summary>
     /// <returns></returns>
-    private bool cooldown;
-    public virtual IEnumerator DamageCooldown()
+    public virtual IEnumerator DamagePlayer()
     {
-        var damage = attackDamage;
-        if (!cooldown)
+        if (!DamageTimeout)
         {
-            var state = State;
-            State = -1;
+            Player.GetComponent<PlayerStats>().PlayerTakesDamage(attackDamage);
+            moving = false;
+            animator.Play("Melee");
+            var damage = attackDamage;
+            moveSpeed = 0;
             attackDamage = 0;
-            cooldown = true;
+            DamageTimeout = true;
             yield return new WaitForSeconds(0.5f);
-            State = state;
+            moveSpeed = firstSpeed;
             yield return new WaitForSeconds(0.5f);
             attackDamage = damage;
-            cooldown = false;
+            DamageTimeout = false;
         }
     }
 }
--- HPTEOL/Assets/Scripts/Enemy/Skeleton.cs
+++ HPTEOL/Assets/Scripts/Enemy/Skeleton.cs
@@ -1,28 +1,15 @@
 using UnityEngine;
+using System.Collections;
 
 public class Skeleton : BaseEnemy
 {
     [Header("Skeleton Stats")]
-    [Range(0, 100)]
-    [SerializeField] int FlameChance = 6;
-
-    private Animator animator;
-
-    public override void Start()
-    {
-        animator = GetComponent<Animator>();
-        base.Start();
-    }
+    [Range(0, 10)]
+    [SerializeField] int FlameChance = 3;
 
     /// <summary>
-    /// Implement skeleton flame
+    /// Implement skeleton flame.
     /// </summary>
-    public override void Target()
-    {
-        if (Random.Range(0, 1000) < FlameChance)
-        {
-            Debug.Log("Flame");
-        }
-        base.Target();
-    }
+
+
 }
